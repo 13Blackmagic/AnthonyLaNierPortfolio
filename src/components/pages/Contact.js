d08dea2722ebc167287c2d291428ec82a3ac11c1
13Blackmagic <123032739+13Blackmagic@users.noreply.github.com>
2023-07-26 20:16:54 -0600

    adding contact info

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -5,7 +5,9 @@ export default function Contact() {
     <div>
       <h1>Contact Page</h1>
       <p>
-      
+      Github:https://github.com/13Blackmagic<br/>
+      Cell phone: 801-520-0321<br/>
+      Email:Anthony.LaNier@outlook.com
       </p>
     </div>
   );
