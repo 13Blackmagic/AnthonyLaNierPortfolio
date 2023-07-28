a0323799914f8fd697cba0a8b464690363585668
13Blackmagic <123032739+13Blackmagic@users.noreply.github.com>
2023-07-27 19:50:19 -0600

    adding to portfolio

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -5,12 +5,13 @@ export default function Portfolio() {
     <div>
       <h1>Portfolio</h1>
       <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+        This page is some of the work that I have done that I am most proud of.
+      </p>
+
+      <p>
+        This is one of my favorite projects. It was really fun to make and I
+        learned a lot about Nosql databases and how to use them.<br/>
+        First Project: <a href="https://github.com/13Blackmagic/Tom-did-nothing-wrong">Tom did nothing wrong</a>
       </p>
     </div>
   );
