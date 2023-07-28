c10d90e668ca8dec3fb8f587ed397dbb44b7129d
13Blackmagic <123032739+13Blackmagic@users.noreply.github.com>
2023-07-27 20:52:39 -0600

    added breaks to about.js

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,13 +5,13 @@ export default function About() {
   return (
     <div>
       <h1>About</h1>
-      <img height="200px" src={process.env.PUBLIC_URL + authorPic} className=" float-left myPic" alt="Anthony" />
+      <img height="400px" src={process.env.PUBLIC_URL + authorPic} className=" float-left myPic" alt="Anthony" />
       <p>
-      Anthony is a full stack web developer with a background in sales and
-        customer service. He recently earned a certificate in full stack web
-        development from the University of Utah. He is a creative problem
-        solver, who is passionate about developing apps, with a focus on mobile
-        first design and development. He is excited to leverage his skills as
+      Anthony is a full stack web developer with a background in sales and <br/>
+        customer service. He recently earned a certificate in full stack web <br/>
+        development from the University of Utah. He is a creative problem <br/>
+        solver, who is passionate about developing apps, with a focus on mobile <br/>
+        first design and development. He is excited to leverage his skills as <br/>
         part of a fast-paced, quality-driven team to build better experiences on
         the web.
       </p>
