d0cb1dcbc942f48fc70d92c8021a255f6e3c5142
13Blackmagic <123032739+13Blackmagic@users.noreply.github.com>
2023-07-30 23:10:07 -0600

    adding photots

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,5 +1,11 @@
 import React from 'react';
-let authorPic = "/assets/images/code quiz.jpg";
+let authorPic = "/assets/images/code quiz.jpeg";
+let authorpic2 = "/assets/images/NoSQL.png";
+let authorpic3 = "/assets/images/express.jpeg";
+let authorpic4 = "/assets/images/README.jpeg";
+let authorpic5 = "/assets/images/shapes.jpeg";
+let authorpic6 = "/assets/images/tom.jpeg";
+
 <link rel="stylesheet" type="text/css" href="../assets/style.css"/>
 export default function Portfolio() {
   return (
@@ -30,11 +36,12 @@ export default function Portfolio() {
         This challenge uses OOP (Object Oriented Programming) and it was really fun to make.<br/>
         OOP Project: <a href="https://github.com/13Blackmagic/Shapes">Shapes</a>
       </p>
+      <image height="200px" src={process.env.PUBLIC_URL + authorpic5} className=" float-left myPic" alt="shapes" />
       <p>
         This was an early project that I did It is a code quiz using javascript.<br/>
         Javascript Project: <a href="https://github.com/13Blackmagic/code-quiz ">Code Quiz</a>
       </p>
-        <img height="400px" src={process.env.PUBLIC_URL + authorPic} className=" float-left myPic" alt="codequiz" />
+        <img height="200px" src={process.env.PUBLIC_URL + authorPic} className=" float-left myPic" alt="codequiz" />
         </div>
   );
 }
