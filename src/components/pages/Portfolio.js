5ead3bb1a893a5d5f6b3d7f503f3fff09fecaba2
13Blackmagic <123032739+13Blackmagic@users.noreply.github.com>
2023-07-30 18:39:13 -0600

    updating portfolio

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
+let authorPic = "/assets/images/code quiz.jpg";
+<link rel="stylesheet" type="text/css" href="../assets/style.css"/>
 export default function Portfolio() {
   return (
     <div>
       <h1>Portfolio</h1>
       <p>
-        This page is some of the work that I have done that I am most proud of.
+      This page is some of the work that I have done that I am most proud of.
       </p>
 
       <p>
@@ -33,7 +34,11 @@ export default function Portfolio() {
         This challenge uses OOP (Object Oriented Programming) and it was really fun to make.<br/>
         OOP Project: <a href="https://github.com/13Blackmagic/Shapes">Shapes</a>
       </p>
-
-    </div>
+      <p>
+        This was an early project that I did It is a code quiz using javascript.<br/>
+        Javascript Project: <a href="https://github.com/13Blackmagic/code-quiz ">Code Quiz</a>
+      </p>
+        <img height="400px" src={process.env.PUBLIC_URL + authorPic} className=" float-left myPic" alt="Anthony" />
+        </div>
   );
 }
